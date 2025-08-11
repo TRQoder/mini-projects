55612a9311bc745ee3cf4732bec36f67f0d04c39
Tarique <mdtariqueh1@gmail.com>
2025-08-12 01:13:53 +0530

    .

--- miniauth/frontend/src/api/axiosConfig.js
+++ miniauth/frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://mini-auth.onrender.com/api/auth/",
+  baseURL: "https://mini-auth-5eyl.onrender.com/auth/",
   //   baseURL: "http://localhost:3000/api/auth",
   withCredentials: true, // ✅ Send cookies with every request
 });
